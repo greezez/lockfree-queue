8ba856c7c5d7dcadecc094b2b69f1e5798bcdafe
Greego <kirill.grigo@gmail.com>
2023-01-17 17:21:35 +0300

    start of work

---
+++ lockfree-queue.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#ifndef NEVGRID_LOCKFREEQUEUE_HPP
+#define NEVGRID_LOCKFREEQUEUE_HPP
+
+namespace nevgrid
+{
+
+
+	namespace details
+	{
+	}
+
+
+	namespace spsc
+	{
+	}
+
+
+	namespace mpsc
+	{
+	}
+
+
+	namespace mpmc
+	{
+	}
+}
+
+#endif // NEVGRID_LOCKFREEQUEUE_HPP
